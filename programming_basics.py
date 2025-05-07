257b974d1b34d4821490cd79a96b885e8883f927
Lilia <rybkalilia8@gmail.com>
2025-05-07 13:34:01 +0300

    Перший коміт

---
+++ programming_basics.py
@@ -0,0 +1,24 @@
+# Умовні оператори
+x = 10
+if x > 0:
+    print("Число додатне")
+elif x == 0:
+    print("Число нуль")
+else:
+    print("Число від'ємне")
+
+# Булеві оператори та складна умова
+age = 20
+student = True
+if age >= 18 and student:
+    print("Повнолітній студент")
+
+# Цикл for
+for i in range(5):
+    print("Цикл for:", i)
+
+# Цикл while
+count = 0
+while count < 3:
+    print("Цикл while:", count)
+    count += 1
